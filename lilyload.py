5d95bbca0e4932747fae49397a46beb76993febb
Carsten Schelp <carsten.schelp@surf.nl>
2023-04-10 18:53:35 +0200

    lilyload v 1.0

--- lilyload.py
+++ lilyload.py
@@ -3,40 +3,50 @@
 import sys
 import os
 import subprocess as sub
+# webdriver for firefox requires geckodriver installed
 from selenium import webdriver as wd
+from time import sleep
 
 if len(sys.argv) != 2:
-    print("Usage: 'lilyload my_lilypond_file' (without '.ly'!)")
-    exit()
+  print("Usage: 'lilyload my_lilypond_file' (without '.ly'!)")
+  exit()
 
-which_process = sub.run(["which", "lilypond"])
-lily_exec = which_process.stdout
+which_process = sub.run(["which", "lilypond"], capture_output=True, text=True)
+lily_exec = which_process.stdout.strip()
 
 filename = sys.argv[1]
 lily_file = f"{filename}.ly"
 lily_path = f"{os.getcwd()}/{filename}.ly"
 pdf_path = f"{os.getcwd()}/{filename}.pdf"
 
-# previous_file_time = now
-# pdf_is_rendered = False
-# while True:
-
 if not os.path.exists(lily_file):
-    print(f"File '{lily_path}' does not exist.")
+  print(f"File '{lily_path}' does not exist.")
+  exit()
 
-# current_file_time = ?
-# if previous_file_time < current_file_time:
-  # compile
-  # render/refresh (pdf_is_rendered)
-# previous_file_time = current_file_time
-# time.sleep(2)
+previous_file_time = os.stat(lily_path).st_mtime
+pdf_is_rendered = False
+driver = wd.Firefox()
 
-lilypond_process = sub.run([lily_exec,  lily_path])
+if os.path.exists(pdf_path):
+  driver.get(f"file://{pdf_path}")
+  pdf_is_rendered = True
 
-if lilypond_process.returncode == 0:
-    driver = wd.Firefox()
-    driver.get(f"file://{pdf_path}")
+while True:
 
+  current_file_time = os.stat(lily_path).st_mtime
+  if previous_file_time < current_file_time:
+    # compile
+    lilypond_process = sub.run([lily_exec,  lily_path])
+    if lilypond_process.returncode == 0:
+      if not pdf_is_rendered:
+        # render
+        driver.get(f"file://{pdf_path}")
+        pdf_is_rendered = True
+      else:
+        # refresh
+        driver.refresh()
+    previous_file_time = current_file_time
+  sleep(1)
 
 
 
